18f8b80c3938080e9a3ad092b7f1c017f9ea1b2f
yaltachen <82513200@qq.com>
2018-05-23 17:03:47 +0800

    add omitempty

--- server/models/carousel.go
+++ server/models/carousel.go
@@ -1,9 +1,9 @@
 package models
 
 type Carousel struct {
-	ID        uint64 `json:"id"`
-	ImgUrl    string `json:"img_url"`
-	ISBN      uint64 `json:"isbn"`
-	IsShowing bool   `json:"is_showing"`
-	AddTime   string `json:"add_time"`
+	ID        uint64 `json:"id,omitempty"`
+	ImgUrl    string `json:"img_url,omitempty"`
+	ISBN      uint64 `json:"isbn,omitempty"`
+	IsShowing bool   `json:"is_showing,omitempty"`
+	AddTime   string `json:"add_time,omitempty"`
 }
